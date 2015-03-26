c15d82548bbdfe3becb8e84a353d8208fb937ccf
haya14busa <hayabusa1419@gmail.com>
2015-03-27 07:03:12 +0900

    add gibo function

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -1,3 +1,5 @@
+function exists { which $1 &> /dev/null }
+
 # load peco sources
 if [ -d ~/.peco/zsh/plugins/ ]; then
   for f (~/.peco/zsh/plugins/*) source "${f}"
@@ -11,3 +13,8 @@ bindkey '^xf' peco_find
 bindkey '^gr' peco-git-recent-remote-branches
 bindkey '^gb' peco-git-recent-branches
 bindkey '^gl' peco-git-log
+
+if exists gibo; then
+  # http://sixeight.hatenablog.com/entry/2015/02/28/225001
+  alias gibol='gibo -l | sed "/=/d" | tr "\t", "\n" | sed "/^$/d" | sort | peco | xargs gibo'
+fi
