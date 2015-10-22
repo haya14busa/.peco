fd26e2e0606beb9a5a1da901fc9452ce00dd0393
haya14busa <hayabusa1419@gmail.com>
2015-10-22 17:29:07 +0900

    make history-candidates uniq

--- zsh/plugins/zsh_history.zsh
+++ zsh/plugins/zsh_history.zsh
@@ -1,7 +1,7 @@
 function peco_select_history() {
     local tac
     exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
-    BUFFER=$(fc -l -n 1 | eval $tac | peco --query "$LBUFFER")
+    BUFFER=$(fc -l -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
     CURSOR=$#BUFFER         # move cursor
     zle -R -c               # refresh
 }
